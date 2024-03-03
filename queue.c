696a7834faf3dc8d886009d81682f7ad7a5638b2
steven523 <asdzxc6pe613@gmail.com>
2024-03-03 20:26:10 +0800

    Implement q_insert_head function
    Create a new `element_t` and allocate memory for it using `malloc`. Then, use the `strdup` function to copy the parameter `s` and allocate memory space for it. Finally, use the `list_add` function from `list.h` to insert the node into the queue.

--- queue.c
+++ queue.c
@@ -38,7 +38,22 @@ void q_free(struct list_head *head)
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
-    return true;
+    if (!head)
+        return false;
+
+    element_t *new = malloc(sizeof(element_t));
+    if (new) {
+        new->value = strdup(s);
+        if (!new->value) {
+            free(new);
+            return false;
+        }
+
+        list_add(&new->list, head);
+        return true;
+    } else {
+        return false;
+    }
 }
 
 /* Insert an element at tail of queue */
