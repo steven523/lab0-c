835184f56048788ce1f080f24eaf59532c3cd137
steven523 <asdzxc6pe613@gmail.com>
2024-03-04 16:55:52 +0800

    Implement q_remove_tail function
    Similar to `q_remove_head`, use `list_last_entry` to retrieve the address of the last node, and remove the tail node using `list_del_init(head->next);`.

--- queue.c
+++ queue.c
@@ -99,7 +99,18 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+
+    element_t *delement = list_last_entry(head, element_t, list);
+    list_del_init(head->prev);
+
+    if (sp) {
+        strncpy(sp, delement->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+
+    return delement;
 }
 
 /* Return number of elements in queue */
